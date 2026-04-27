a04d488eab8d5fb22f7def02bc6ce43459f087d2
Kartikey-T <ktindia2005@gmail.com>
2026-04-27 21:38:52 +0530

    Fix button redirect URL in MapScreen

--- src/screens/MapScreen.jsx
+++ src/screens/MapScreen.jsx
@@ -107,7 +107,7 @@ function MapScreen({ territories, onGoToMenu, onAttack }) {
           className="secondary-button"
           style={{ marginBottom: '14px' }}
           onClick={() => {
-            window.location.href = 'public/index.html'
+            window.location.href = '/index.html'
           }}
         >
           Enter Duel Arena ⚔️
